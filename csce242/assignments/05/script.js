ec0e0f81d6973ab6fcf3e117dd5101e96653b1a6
mad1826 <mad18@email.sc.edu>
2024-09-12 16:42:58 -0400

    Commit assignment 5

---
+++ csce242/assignments/05/script.js
@@ -0,0 +1,14 @@
+let count = 0;
+
+document.getElementById('count-container').onclick = () => {
+	count++;
+	document.getElementById('count').innerHTML = count.toString();
+};
+
+document.getElementById('random-img').onclick = () => {
+	location.reload();
+};
+
+document.getElementById('myRange').oninput = (ev) => {
+	document.getElementById('movable-img').style.left = `${ev.target.value / 2}%`
+}
